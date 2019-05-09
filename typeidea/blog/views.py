e731a44d64ef1f9d5116cc86dc25b634537a5af6
Unknown <1009304154@qq.com>
2019-05-09 14:11:51 +0800

    新增了访问统计

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,4 +1,7 @@
-from django.db.models import Q
+from datetime import date
+
+from django.core.cache import cache
+from django.db.models import F, Q
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
@@ -23,6 +26,35 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'  # 指定模板页面
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'  # url传进来的参数
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        # pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        # uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+        pv_key = 'pv:{}:{}'.format(uid, self.request.path)
+        uv_key = 'uv:{}:{}:{}'.format(uid, str(date.today()), self.request.path)
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1*60)  # 1分钟有效
+
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24*60*60)  # 24小时有效
+
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
+
 
     # def get_context_data(self, **kwargs):
     #     # 重写get_context_data方法
